6e28e342590bfae90fec1fdd735ebfb4dddcb3e2
MuntYounis <muntaseryounis60@gmail.com>
2024-09-09 00:19:08 -0400

    react challenge

---
+++ src/components/Player.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export default function Player() {
+  const [player, setPlayer] = useState({
+    firstName: 'Ranjani',
+    lastName: 'Shettar',
+    score: 10,
+  });
+
+  function handlePlusClick() {
+    setPlayer({
+      ...player, // Spread the existing player object
+      score: player.score + 1, // Update the score
+    });
+  }
+
+  function handleFirstNameChange(e) {
+    setPlayer({
+      ...player,
+      firstName: e.target.value,
+    });
+  }
+
+  function handleLastNameChange(e) {
+    setPlayer({
+      ...player, // Make sure to spread the existing state to avoid losing other properties
+      lastName: e.target.value,
+    });
+  }
+
+  return (
+    <>
+      <label>
+        Score: <b>{player.score}</b>
+        {' '}
+        <button onClick={handlePlusClick}>
+          +1
+        </button>
+      </label>
+      <label>
+        First name:
+        <input
+          value={player.firstName}
+          onChange={handleFirstNameChange}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          value={player.lastName}
+          onChange={handleLastNameChange}
+        />
+      </label>
+    </>
+  );
+  }
